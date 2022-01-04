a3605aeb67a6a06c0acfadd29c2fe549c617078e
Sefa Keles <sefaaakeles@gmailcom>
2022-01-04 16:29:58 +0000

    Folder Structures and packages are created

---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export class Navbar extends Component {
+    render() {
+        return (
+            <div>
+                Navbar
+            </div>
+        )
+    }
+}
+
+export default Navbar
+
